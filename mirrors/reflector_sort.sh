1d06cf21c5c600f335659fc31301c26cd590fbda
Rodrigo Bustamante <bustamanteroddrigo@gmail.com>
2023-09-14 14:20:31 -0600

    Agre set to 6

--- mirrors/reflector_sort.sh
+++ mirrors/reflector_sort.sh
@@ -14,4 +14,4 @@ case $country_choice in
     ;;
 esac
 
-reflector --verbose --age 24 $protocol $countries --sort rate --save $reflector_tmp
+reflector --verbose --age 6 $protocol $countries --sort rate --save $reflector_tmp
